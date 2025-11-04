18f5a8920d3292c8056de63284b1291b70acb64f
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-11-04 20:56:26 +0800

    menambahkan header dan footer

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</main> <footer class="text-center mt-5 mb-3">
+  <p>&copy; 2025 - <?php echo date("Y"); ?> InventarisKU</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
